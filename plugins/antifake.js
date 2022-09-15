ce40ce8cdeec5c5b26d27bd3b900b3dce513282f
DarkenWaves <108982982+DarkenWaves@users.noreply.github.com>
2022-09-15 21:06:13 +0200

    Add files via upload

--- plugins/antifake.js
+++ plugins/antifake.js
@@ -1,17 +1,17 @@
 const {Function,setAntiFake,antiFakeList,prefix} = require('../lib/')
 const {getFake} = require('../lib/database/antifake')
-Function({pattern: 'antifake ?(.*)', fromMe: true, desc: 'set antifake', type: 'group'}, async (m, match) => {
+Function({pattern: 'kickfilter ?(.*)', fromMe: true, desc: 'set antifake', type: 'group'}, async (m, match) => {
 if (!m.isGroup) return await m.reply('_This command only works in group chats_')
 const groupMetadata = await m.client.groupMetadata(m.chat)
 const isAntiFake = await getFake(m.jid)
 let buttons = [
-  {buttonId: prefix + 'antifake on', buttonText: {displayText: 'ON'}, type: 1},
-  {buttonId: prefix + 'antifake off', buttonText: {displayText: 'OFF'}, type: 1},
-  {buttonId: prefix + 'antifake list', buttonText: {displayText: 'LIST'}, type: 1}
+  {buttonId: prefix + 'kickfilter enable', buttonText: {displayText: 'enable'}, type: 1},
+  {buttonId: prefix + 'kickfilter disable', buttonText: {displayText: 'disable'}, type: 1},
+  {buttonId: prefix + 'kickfilter check', buttonText: {displayText: 'check'}, type: 1}
 ]
 let isantiFake = isAntiFake.enabled || false
 const buttonMessage = {
-text: 'Antifake Manager',
+text: 'kickfilter Manager',
 footer: 'Group Name : ' + groupMetadata.subject + '\nAntiFake status : ' + isantiFake,
 buttons: buttons,
 headerType: 1
@@ -21,10 +21,10 @@ if (match == 'list') {
 if (!isAntiFake) return await m.reply("_You don't set the Antifake yet.!_\n__To set:__ ```.antifake 1,44,972...```")
 return await m.reply(await antiFakeList(m.jid))
 }
-if (match == 'on' || match == 'off') {
+if (match == 'enable' || match == 'disable') {
 await setAntiFake(m.jid, match)
-return await m.reply(`_Antifake ${match == 'on' ? 'Activated' : 'Deactivated'}_`)
+return await m.reply(`_Antifake ${match == 'enable' ? 'Aktiviert' : 'Deaktiviert'}_`)
 }
 await setAntiFake(m.jid, match)
-return await m.reply('_Antifake Updated_')
+return await m.reply('_Kickfilter hinzugefügt_')
 })
